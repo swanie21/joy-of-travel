96059243ad80bbb76e0d8061157c3dbfe87fac20
Kirsten Barton <kirsten.swanson@brandfolder.com>
2022-05-08 09:11:03 -0700

    add location markers of countries I have traveled to

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -5,6 +5,7 @@ import { mapboxClient } from '../../services/mapbox';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+import { markers } from './Markers';
 import styles from './map.module.scss';
 
 const Map: NextPage = () => {
@@ -43,6 +44,41 @@ const Map: NextPage = () => {
         zoom: 2.1
       });
     }
+
+    const geojson = {
+      type: 'FeatureCollection',
+      features: markers.map((marker) => ({
+        properties: {
+          iconSize: [30, 42],
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: {
+            lng: marker.longCoord,
+            lat: marker.latCoord
+          }
+        }
+      }))
+    };
+
+    map.current.on('load', () => {
+      geojson.features.forEach((marker) => {
+        // create a DOM element for the marker
+        const markerIcon = document.createElement('div');
+        markerIcon.className = 'events-marker';
+        markerIcon.style.backgroundImage = 'url(/location-marker.png)';
+        markerIcon.style.width = marker.properties.iconSize[0] + 'px';
+        markerIcon.style.height = marker.properties.iconSize[1] + 'px';
+
+        // add marker to map
+        new mapboxgl.Marker(markerIcon)
+          .setLngLat(marker.geometry.coordinates)
+          .addTo(map.current);
+      });
+    });
+
+    // disable map zoom when scrolling
+    map.current.scrollZoom.disable();
 
     // Add zoom and rotation controls to the map
     map.current.addControl(new mapboxgl.NavigationControl());
